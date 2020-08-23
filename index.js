35dc606cf1bffc3eeb1cba9cad9ef9cdf48543a6
EASYMAK777 <jplocallawncare@gmail.com>
2020-08-22 23:32:04 -0400

    installed utils node module

--- index.js
+++ index.js
@@ -3,19 +3,3 @@ const fs = require("fs");
 const util = require("util");
 const { type } = require('os');
 
-const writeFileAsync = util.promisify(fs.writeFile);
-
-
-fucntion promptUser(){
-    return inquirer.prompt([{
-
-        type: "input",
-        message: "What is the name of your project?",
-        name: "title"
-    }
-    ])
-}
-
-
-
-
