eec6143e8bc062d6d6efaad32d7f34871085c827
EASYMAK777 <jplocallawncare@gmail.com>
2020-08-22 23:44:31 -0400

    added util module, added promisify function

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const inquirer = require('inquirer');
 const fs = require("fs");
 const util = require("util");
-const { type } = require('os');
+const writeFileAsync = util.promisify(fs.writeFile);
 
