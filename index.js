752c4463fdafef3825fdce5cdaff464932e60139
EASYMAK777 <jplocallawncare@gmail.com>
2020-08-22 23:48:21 -0400

    testing

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const fs = require("fs");
 const util = require("util");
 const writeFileAsync = util.promisify(fs.writeFile);
 
+console.log(inquirer);
+console.log(fs);
+console.log(util);
+console.log(writeFileAsync);
+
