ceb6732204b6e835f0f6f5a82c509e14ebc45c52
EASYMAK777 <jplocallawncare@gmail.com>
2020-08-22 22:22:24 -0400

    added inquire var, fs var, util var,

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log(process.argv);
+const inquirer = require('inquirer');
+const fs = require("fs");
+const util = require("util");
+const { type } = require('os');
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+
+fucntion promptUser(){
+    return inquirer.prompt([{
+
+        type: "input",
+        message: "What is the name of your project?",
+        name: "title"
+    }
+    ])
+}
+
+
+
+
