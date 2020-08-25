6eb222f269cc4ad27767c90f279de9eac4038be7
EASYMAK777 <jplocallawncare@gmail.com>
2020-08-24 22:41:47 -0400

    added more questions for readme gen

--- index.js
+++ index.js
@@ -58,58 +58,84 @@
 var inquirer = require("inquirer");
 
 inquirer
-  .prompt([
-    {
-      type: "input",
-      message: "What is your project called?",
-      name: "name"
-    },
-    {
-      type: "input",
-      message: "Please enter a description of your project?",
-      name: "description"
-    },
-    {
-      type: "input",
-      message: "Enter your table of contents",
-      name: "table of contents"
-    },
+    .prompt([
+        {
+            type: "input",
+            message: "What is your project called?",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "Please enter a description of your project?",
+            name: "description"
+        },
+        {
+            type: "input",
+            message: "Enter your table of contents",
+            name: "table of contents"
+        },
 
-    {
-        type: "input",
-        message: "Who are the contributers for this project?",
-        name: "contributors"
-    },
+        {
+            type: "input",
+            message: "Who are the contributers for this project?",
+            name: "contributors"
+        },
 
-    {
-        type: "input",
-        message: "Please enter installation instructions",
-        name: "installation"
-    },
+        {
+            type: "input",
+            message: "Please enter installation instructions",
+            name: "installation"
+        },
 
-    {
-        type: "input",
-        message: "Installation",
-        name: "confirm"
-    },
+        {
+            type: "input",
+            message: "How do you want your application to be used?",
+            name: "usage"
+        },
 
-    {
-        type: "input",
-        message: "Installation",
-        name: "confirm"
-    },
+        {
+            type: "checkbox",
+            message: "Please select a license.",
+            choices: ["ISC",
+                "MIT",
+                "GNU GPLv3"
+            ],
+            name: "license"
+        },
+
+        {
+            type: "input",
+            message: "Enter the test instructions for this app",
+            name: "tests"
+        },
+
+        {
+            type: "input",
+            message: "What is your GitHub username?",
+            name: "username"
+        },
+        {
+
+            type: "input",
+            message: "What is the github pages link for this project?",
+            name: "link"
+
+
+        },
+
+        
 
 
 
 
 
-  ])
-  .then(function(response) {
+    ])
+    .then(function (response) {
 
-    if (response.confirm === response.password) {
-      console.log("Success!");
-    }
-    else {
-      console.log("You forgot your password already?!");
-    }
-  });
+        if (response.confirm === response.password) {
+            console.log("Success!");
+        }
+        else {
+            console.log("You forgot your password already?!");
+        }
+    });
